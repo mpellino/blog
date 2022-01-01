54312456cb62b7cd8d4f3ab551cdb9ed041844a3
mpellino <marcopellino00@gmail.com>
2022-01-01 21:17:41 +0100

    Add individual blog pages.
    1) Create a new view with DetailView class ( see view.py for comments )
    2) Add url
    3) Add html templates
    4) Add hyperlinks

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
@@ -8,3 +8,15 @@ from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+
+'''
+Create a DetailView class.
+in this view we define the model we are using, Post, and the template we want to associate with, post.detail.html
+by Defaul DetailView provides a context object we can use in our template, called object or the name of our 
+model in lowercase. ( in this case it is : post)
+'''
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
